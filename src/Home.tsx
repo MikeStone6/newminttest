9cc462365404573600c671e8b7b42a1f9f094a01
Pappie <stonescod3@gmail.com>
2023-01-26 21:30:56 +0200

    Moving NFTS

--- src/Home.tsx
+++ src/Home.tsx
@@ -403,36 +403,7 @@ const Home = (props: HomeProps) => {
               setMintedItems={setMintedItems}
             />
           </StyledContainer>
-          <NftWrapper>
-            <div className="marquee-wrapper">
-              <div className="marquee">
-                {[...Array(21)].map((item, index) => (
-                  <img
-                    key={index}
-                    src={`/nfts/${index + 1}.jpeg`}
-                    height="200px"
-                    width="200px"
-                    alt=""
-                  />
-                ))}
-              </div>
-            </div>
-          </NftWrapper>
-          <NftWrapper2>
-            <div className="marquee-wrapper second">
-              <div className="marquee">
-                {[...Array(21)].map((item, index) => (
-                  <img
-                    key={index}
-                    src={`/nfts/${index + 1}.jpeg`}
-                    height="200px"
-                    width="200px"
-                    alt=""
-                  />
-                ))}
-              </div>
-            </div>
-          </NftWrapper2>
+         
         </Root>
       </>
       <Snackbar
